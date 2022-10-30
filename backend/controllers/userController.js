afea35c5a89c3b828570c7ddcf9b91bde71b535d
unknown <minhtu2005ha@gmail.com>
2022-10-30 21:36:39 +0700

    add backend-3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -120,13 +120,81 @@ const registerUser = asyncHandler( async (req, res) => {
    
     //Get User Data
     const getUser = asyncHandler (async(req, res) => {
-        res.send("getUser")
+        const user = await User.findById(req.user._id)
+        const { _id, name, email, photo, phone, bio } = user
+        if (user) {
+            res.status(200).json({
+                _id, name, email, photo, phone, bio,
+            })
+        } else {
+            res.status(400);
+            throw new Error("User not found")
+        }
     })
 
+    //Logined Status
+    const loginStatus = asyncHandler (async(req, res) => {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.json(false);
+        }
+            // Verify Token
+            const veridied = jwt.verify(token, process.env.JWT_SECRET)
+        if (veridied) {
+            return res.json(true);
+        }
+        return res.json(false);
+    })
+
+    //Updated User
+    const updateUser = asyncHandler (async(req, res) => {
+        const user = await User.findById(req.user._id)
+        const { name, email, photo, phone, bio } = user
+        if (user) {
+            user.email = email;
+            user.name = req.body.name || name;
+            user.photo = req.body.photo || photo;
+            user.phone = req.body.phone || phone;
+            user.bio = req.body.bio || bio;
+            
+            const updateduser = await user.save();
+            res.status(200).json({
+                _id: updateduser._id,
+                name: updateduser.name,
+                email: updateduser.email,
+                photo: updateduser.photo,
+                phone: updateduser.phone,
+                bio: updateduser.bio,
+            })
+        } else {
+            res.status(404)
+            throw new Error("User not found!")
+        }
+    })
+
+    //Changed Password
+    const changePassword = asyncHandler (async(req, res) => {
+        const user = await User.findById(req.user._id)
+        const { oldPassword, password } = res.body
+
+        if (!user) {
+            res.status(400);
+            throw new Error("User not found, Please sign up ");
+        }
+
+        if (!oldPassword || !password) {
+            res.status(400);
+            throw new Error("Please add new and old password ");
+        }
+
+    })
 
 module.exports = {
     registerUser,
     loginUser,
     logout,
-    getUser
+    getUser,
+    loginStatus,
+    updateUser,
+    changePassword
 }
